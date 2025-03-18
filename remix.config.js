1e37c26c4e909c6c22cce1050d170a1e9f17c97a
Justin Hunter <justin@pinata.cloud>
2025-03-18 11:54:30 -0500

    added remix config file

---
+++ remix.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  // This will force Remix to include all modules in the server bundle
+  serverDependenciesToBundle: 'all',
+
+  // Your other Remix config options...
+};
